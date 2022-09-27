d8f4b0a84b05eced34c43ce77bef212b7bb4ef61
omkhard <omkhard09@gmail.com>
2022-09-27 06:27:20 -0400

    Changed the for loop efficient

--- infoGather.py
+++ infoGather.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
 # this is my Information Gathering miniProject
+#
 import socket
 import requests
 import sys , os
-import urllib.request
+import urllib.request 
+import json
+
 
 try:
 	os.system("figlet infoGather") #created  in exception for some users where figlet isnot present
@@ -30,23 +34,35 @@ req = None #no requests
 if sock.fileno() != -1: # if domain is up  , then analyse further
 	print(f"\033[32mDomain {socket.gethostbyname(domain)} is Up... \033[37m")
 	if port == 443:
-		req = requests.get(f"https://{domain}")
+		req = requests.get(f"https://{domain}",verify=False)
 		print(f"Status Code : {req.status_code}")
 		print(f"Service by port : {socket.getservbyport(port)}")
 
 	if port == 80:
-		req = requests.get(f"http://{domain}:{port}")
+		req = requests.get(f"http://{domain}:{port}",verify=False)
 		print(f"Status Code : {req.status_code}")
 		print(f"Service by port : {socket.getservbyport(port)}")
 
 print(f"Service by : {req.headers['Server']}")
-for i in range(0,1000):
+
+# trying to look for only tcp ports and not "to check with bruteforcing"
+f = open("../ports/ports.json")
+data = json.load(f)
+ports = []
+for key,value in data.items():
+	port = ''.join(key.split('/')[0])
+	if int(port)<2049:
+		ports.append(port)
+	else:
+		break
+
+for i in set(ports):
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # only dicovers TCP ports i repeat only discovers TCP ports
 	s.settimeout(0.2) 
-	result = s.connect_ex((domain,i))
+	result = s.connect_ex((domain,int(i)))
 	if result == 0:
 		print(f"Discovered port {i}")
-      
+
 # request's  headers 
 for i in req.headers:
 	print(req.headers[i])
